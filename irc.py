401686be76b6bcc712a6c52a5de4ba67d6323cd1
fwilson <fwilson@fwilson.me>
2014-01-23 15:58:32 -0500

    irc

---
+++ irc.py
@@ -0,0 +1,26 @@
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+
+class ClosingBot(irc.IRCClient):
+
+    nickname = "closures"
+    channel = "#closures"
+
+    def signedOn(self):
+        self.join(self.channel)
+
+    def a
+   
+class ClosingFactory(protocol.ClientFactory):
+   
+    def buildProtocol(self, addr):
+        import data
+        p = ClosingBot()
+        p.data = data.ClosingsData()
+        p.data.bot = p
+        return p
+
+if __name__ == '__main__':
+    f = ClosingFactory()
+    reactor.connectTCP("chat.freenode.org", 6667, f)
+    reactor.run()
